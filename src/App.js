0ec7fe89fd702fcdd92b48609ca7211c0e8bcb25
Ivelin Dimitrov <ivelin.dimitrov966@gmail.com>
2018-10-07 18:59:34 +0300

    Added the API key and setup

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 import './App.css';  
 import 'tachyons';
+
+const app = new Clarifai.App({
+  apiKey: 'efcd035226db47089ad8ab69c87ba826'
+ });
 
 const particleOptions = {
   particles: {
@@ -21,6 +26,29 @@ const particleOptions = {
 };
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,7 +59,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm  onInputChange={this.onInputChange} 
+                        onButtonSubmit={this.onButtonSubmit} 
+
+                        />
         {
        /*  
         <FaceRecognition /> */}
